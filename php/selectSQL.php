381ece0e416f8cd2594e476414a5fbfe658afab6
kshpt9043 <kshpt9043@gmail.com>
2025-04-01 14:00:35 +0900

    c

--- php/selectSQL.php
+++ php/selectSQL.php
@@ -1,29 +1,29 @@
 <?php header('Content-Type: text/html; charset=euc-kr'); ?>
 <?php 
-	// ���� ���������� ���� ���� �޽��� Ȯ��
+	// ???? ?????????? ???? ???? ????? ???
 	$message =  $_POST['message'];
 	$message = ( ( ( $message == null ) || ( $message == "" ) || ( strncmp( $message, " * ", 3 ) == 0 ) ) ? "_%" : $message );
 
-	// MySQL ����̹� ���� 
+	// MySQL ?????? ???? 
 	include("./SQLconstants.php");
 	$conn = mysqli_connect($mySQL_host,$mySQL_id,$mySQL_password,$mySQL_database) or die ("Can't access DB");
 	mysqli_query($conn, "SET NAMES 'euckr'");
 	mysqli_set_charset($conn, "euckr");
 
-	// MySQL å �˻� ���� �� ��� ���
+	// MySQL a ??? ???? ?? ??? ???
 	$query = "select * from game where title like '%".$message."%';";
 	$resultSet = mysqli_query( $conn, $query );
 	while( $result = mysqli_fetch_array( $resultSet ) )
 	{
 		echo "\n<BR>ID : ".$result['id'];
-		echo "\n<BR>�����̸� : ".$result['title'];
-		echo "\n<BR>���ۻ� : ".$result['developer'];
-		echo "\n<BR>����� : ".$result['date'];
-		echo "\n<BR>���� : ".$result['price'];
+		echo "\n<BR>??????? : ".$result['title'];
+		echo "\n<BR>????? : ".$result['developer'];
+		echo "\n<BR>????? : ".$result['date'];
+		echo "\n<BR>???? : ".$result['price'];
 		echo "\n<BR><img src = '".$result['image']."' height='280' width='180'> <br>";
 	}
 
-	// MySQL ����̹� ���� ����
+	// MySQL ?????? ???? ????
 	mysqli_free_result( $resultSet );
 	mysqli_close( $conn );
 ?>
